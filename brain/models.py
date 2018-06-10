88379df06c33beae7be583d0f1d07e8e2a549e93
orodriguesjr <junior18playsz@hotmail.com>
2018-06-10 15:07:59 -0300

    dois

--- brain/models.py
+++ brain/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Clinica(models.Model):
+	cnpj = models.CharField(max_length = 45, primary_key = True)
+	nome = models.CharField(max_length = 45)
+	endereco = models.CharField(max_length = 45)
+	contato = models.CharField(max_length = 45)
+
+class Admin(models.Model):
+	cpf = models.CharField(max_length = 45,primary_key = True)
+	nome = models.CharField(max_length = 45)
+	contato = models.CharField(max_length = 45)
+	cnpj_clinica = models.ForeignKey(cnpj_clinica, on_delete=models.CASCADE)
+
+class Paciente(models.Model):
+	cpf = models.CharField(max_length = 45, primary_key = True)
+	nome = models.CharField(max_length = 45)
+	historico = models.CharField(max_length = 45)
+
+class Profissional(models.Model):
+	cpf = models.CharField(max_length = 45, primary_key = True)
+	nome = models.CharField(max_length = 45)
+	contato = models.CharField(max_length = 45)
+	cnpj_clinica = models.ForeignKey(cnpj_clinica, on_delete=models.CASCADE)
+
+class Trabalha_Em(models.Model):
+	cnpj_clinica = models.ForeignKey(cnpj_clinica, on_delete=models.CASCADE)
+	cpf_profissional = models.ForeignKey(cpf_profissional, on_delete=models.CASCADE)
