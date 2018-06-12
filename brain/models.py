b9103f2290193dd4288e8b81b8de1feb66044653
orodriguesjr <junior18playsz@hotmail.com>
2018-06-11 22:37:26 -0300

    incremento no models

--- brain/models.py
+++ brain/models.py
@@ -16,13 +16,15 @@ class Paciente(models.Model):
 	cpf = models.CharField(max_length = 45, primary_key = True)
 	nome = models.CharField(max_length = 45)
 	historico = models.CharField(max_length = 45)
-
 class Profissional(models.Model):
 	cpf = models.CharField(max_length = 45, primary_key = True)
 	nome = models.CharField(max_length = 45)
 	contato = models.CharField(max_length = 45)
+	especialidade = models.CharField(max_length = 45)
 	cnpj_clinica = models.ForeignKey(Clinica, on_delete=models.CASCADE)
 
+
 class Trabalha_Em(models.Model):
+	funcao = models.CharField(max_length = 45)
 	cnpj_clinica = models.ForeignKey(Clinica, on_delete=models.CASCADE)
 	cpf_profissional = models.ForeignKey(Profissional, on_delete=models.CASCADE)
