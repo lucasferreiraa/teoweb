a5c4ecbaa38c494e988f923d1a41e8ec580bf6d2
karlissonJR <karlissonj@gmail.com>
2018-10-25 11:06:35 -0300

    organizando

--- teoweb/urls.py
+++ teoweb/urls.py
@@ -1,7 +1,7 @@
 """teoweb URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import urls
+from teoweb import views
 
 urlpatterns = [
-    path('', include('brain.urls')),
-    path('contas/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('usuario/', include('usuario.urls')),
+    path('clinica/', include('clinica.urls')),
+    path('paciente/', include('paciente.urls')),
+    path('profissional/', include('profissional.urls')),
 ]
