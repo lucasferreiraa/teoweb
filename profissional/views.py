d71259bc5b9770125e3f91e4841ba6547c22e611
karlissonJR <karlissonj@gmail.com>
2018-12-13 19:31:26 -0300

    :)

--- profissional/views.py
+++ profissional/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from profissional.models import Profissional
 from profissional.forms import Novo_Profissional_Form, Atualizar_Profissional_Form
+from clinica.models import Clinica
 
 @login_required
 def novo_profissional(request):
     form = Novo_Profissional_Form(request.POST or None)
+    clinicas = Clinica.objects.all()
 
     if form.is_valid():
         form.save()
         return redirect("listar_profissionais")
 
-    context = {'form': form}
+    context = {'form': form, 'clinicas': clinicas}
     return render(request, "novo-profissional.html", context)
 
 @login_required
@@ -26,12 +28,13 @@ def listar_profissional(request):
 def atualizar_profissional(request, cpf):
     profissional = Profissional.objects.get(cpf=cpf)
     form = Atualizar_Profissional_Form(request.POST or None, instance=profissional)
+    clinicas = Clinica.objects.all()
 
     if (form.is_valid()):
         form.save()
         return redirect("listar_profissionais")
 
-    context = {'profissional':profissional, 'form':form}
+    context = {'profissional':profissional, 'form':form, 'clinicas': clinicas}
     return render(request, "novo-profissional.html", context)
 
 @login_required
