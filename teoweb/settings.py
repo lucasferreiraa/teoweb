a5c0e9d25cad09ffdf1a35c4ef92ec6a9677d04a
karlissonJR <karlissonj@gmail.com>
2019-05-27 18:12:08 -0300

    perfil-prontuario

--- teoweb/settings.py
+++ teoweb/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'teo_web',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
 
